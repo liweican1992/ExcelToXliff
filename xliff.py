701704062125e65dcbb0f5bb3a2af207c024972e
liweican <liwc@rd.netease.com>
2021-09-08 15:55:57 +0800

    Feat:支持批量导入

--- xliff.py
+++ xliff.py
@@ -1,6 +1,7 @@
+# -*- coding: utf-8 -*-
+
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
 import keyword
 import xlrd
 import xml.etree.ElementTree as ET
@@ -11,24 +12,32 @@ import os
 ns = dict(xliffNameSpace='urn:oasis:names:tc:xliff:document:1.2')
 
 # excel地址
-excelPath = r'/Users/cc/Downloads/One所有文案.xlsx'
+excelPath = os.path.join(os.path.expanduser("~"), 'Desktop/localizable.xlsx')
 
-# xliff地址
-xliffPath = r'/Users/cc/Desktop/UDictionary/id.xcloc/Localized Contents/id.xliff'
+xliffRootPath = os.path.join(os.path.expanduser("~"), 'Desktop/exportLoaclization')
+
+languages = {"Spanish" : "es", "Portuguese" : "pt", "Indonesian" : "id", "Arabic" : "ar", "Japanese" : "ja", "Vietnam" : "vi-VN"}
 
 # 目标语言 需要和excel中一致
-targetName = "Indonesian"
+targetName = ""
+sourceName = "English"
 
 # sheetName 从哪个表读取，找不到会默认读取第一个表
-sheetName = "sheet1"
+sheetName = "ios"
+
+def GetDesktopPath():
+    return os.path.join(os.path.expanduser("~"), 'Desktop')
 
 def readExcel():
+   
     if not os.path.exists(excelPath):
         print('excel路径不存在')
         return
-    if not os.path.exists(xliffPath):
-        print('xliff路径不存在')
-        return
+
+    # if not os.path.exists(xliffPath):
+    #     print('xliff路径不存在')
+    #     return
+
     data = xlrd.open_workbook(excelPath)
     # 拿到索引 默认是在sheet0
     sheet1 = data.sheet_by_index(0)
@@ -37,39 +46,52 @@ def readExcel():
     else:
         print("sheetName不存在 默认读取第一个")
 
-    # 在这里修改语言
-    sourceName = "English"
+
+    # 找English索引
     sourceIndex = -1
-    targetIndex = -1
     # 确定Index
     row_0 = sheet1.row_values(0)
     for k in range(len(row_0)):
         if row_0[k] == sourceName:
             sourceIndex = k
             print(sourceName + " 所在索引 " + str(k))
-        elif row_0[k] == targetName:
-            targetIndex = k
-            print(targetName + " 所在索引 " + str(k))
 
     if sourceIndex == -1:
         print('未找到English Index')
         return
-    if targetIndex == -1:
-        print('未找到 ' + targetName + ' Index')
-        return
-    # 遍历字典
-    dict = {}
-    for i in range(sheet1.nrows):
-        row_value = sheet1.cell_value(i, targetIndex)
-        row_key = sheet1.cell_value(i, sourceIndex)
-        dict[row_key] = row_value
-    print('excel数据读取完毕，共 ', len(dict), ' 条数据')
-    # 读取Xliff
-    writeXliff(dict)
 
 
-def writeXliff(dict):
-    print('读取xliff文件')
+    #找target索引
+    for subLan in languages:
+        print(subLan)
+        targetName = subLan
+        targetIndex = -1
+        for k in range(len(row_0)):
+            if row_0[k] == targetName:
+                targetIndex = k
+                print(targetName + " 所在索引 " + str(k))
+                # 遍历字典
+                dict = {}
+                for i in range(sheet1.nrows):
+                    row_value = sheet1.cell_value(i, targetIndex)
+                    row_key = sheet1.cell_value(i, sourceIndex)
+                    if row_key == 'English' or row_key == "":
+                        continue
+                    dict[row_key] = row_value
+                print('excel数据读取完毕，共 ', len(dict), ' 条数据')
+                print(dict)
+                # 读取Xliff
+                writeXliff(dict,targetName)
+
+
+
+
+
+def writeXliff(dict, targetName):
+    print('读取'+targetName+'xliff文件')
+    lan = languages[targetName]
+    xliffPath = os.path.join(xliffRootPath, lan+'.xcloc/Localized Contents/'+lan+'.xliff')
+    print(xliffPath)
     # 重写nameSpace
     ET.register_namespace('', 'urn:oasis:names:tc:xliff:document:1.2')
     # 拿到根节点
@@ -78,15 +100,16 @@ def writeXliff(dict):
     # print(root.tag)
     count = 0
     for file in root.findall('xliffNameSpace:file', ns):
-        print('读取子文件：' + file.attrib['original'])
+        #print('读取子文件：' + file.attrib['original'])
         body = file.find('xliffNameSpace:body', ns)
         for unit in body.findall('xliffNameSpace:trans-unit', ns):
             source = unit.find('xliffNameSpace:source', ns)
-            # print(source.text)
+            #print(source.text)
             target = unit.find('xliffNameSpace:target', ns)
             # target不存在就创建 在excel中填写译文 否则写入原文
             if target is None:
                 # 创建target
+                print("target为空")
                 node = ET.SubElement(unit, "target")
                 if source.text in dict:
                     node.text = dict[source.text]
